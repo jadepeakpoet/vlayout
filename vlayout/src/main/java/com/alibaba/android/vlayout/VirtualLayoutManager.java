86c5a6df60f692e2b76b493bcf2a01170d5c419b
Jade Peak <1045456217@qq.com>
2022-10-28 10:47:04 +0800

    Migrate to androidx and update plugin version

--- vlayout/src/main/java/com/alibaba/android/vlayout/VirtualLayoutManager.java
+++ vlayout/src/main/java/com/alibaba/android/vlayout/VirtualLayoutManager.java
@@ -28,10 +28,10 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Trace;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.OrientationHelper;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.OrientationHelper;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Pair;
@@ -59,10 +59,10 @@ import java.util.Map;
 
 
 /**
- * A {@link android.support.v7.widget.RecyclerView.LayoutManager} implementation which provides
+ * A {@link RecyclerView.LayoutManager} implementation which provides
  * a virtual layout for actual views.
  * <p>
- * NOTE: it will change {@link android.support.v7.widget.RecyclerView.RecycledViewPool}
+ * NOTE: it will change {@link RecyclerView.RecycledViewPool}
  * for RecyclerView.
  *
  * @author villadora
